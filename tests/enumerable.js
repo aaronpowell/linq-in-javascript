48a94287773fb0cfa968118b0a41d80cba60ed55
Aaron Powell <me@aaron-powell.com>
2013-07-20 14:06:55 +1000

    implementing all tests and moving prime number generator to its own test

--- tests/enumerable.js
+++ tests/enumerable.js
@@ -37,22 +37,5 @@ describe('Enumerable', function () {
                 index++;
             }
         });
-
-        it('should calc prime numbers', function () {
-            var range = Enumerable.range(3, 10);
-
-            var primes = range
-                .where(n =>
-                    Enumerable.range(2, Math.floor(Math.sqrt(n)))
-                        .all(i => n % i > 0)
-                );
-
-            var expectedPrimes = [3, 5, 7];
-            var index = 0;
-            for (let prime in primes) {
-                expect(prime).to.equal(expectedPrimes[index]);
-                index++;
-            }
-        });
     });
 });
