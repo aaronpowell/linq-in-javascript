c8d7db88af70a17d094c5954a9cedb90b966f914
Aaron Powell <me@aaron-powell.com>
2015-05-01 13:44:32 +1000

    adding test running to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
-var rename = require('gulp-rename')
+var rename = require('gulp-rename');
+var mocha = require('gulp-mocha');
 
 gulp.task('watch', function () {
-    gulp.watch('./src/linq.js', ['build', 'build-es5']);
+    gulp.watch('./src/linq.js', ['build', 'build-es5', 'tests']);
 });
 
 gulp.task('build', function () {
@@ -16,6 +17,20 @@ gulp.task('build', function () {
         }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('tests', function () {
+    require('./test-helpers/_helper');
+    require('./test-helpers/_es6');
+    return gulp.src('./test/*.js', { read: false })
+        .pipe(mocha());
+});
+
+gulp.task('tests-es5', function () {
+    require('./test-helpers/_helper');
+    require('./test-helpers/_es6');
+    return gulp.src('./test/*.js', { read: false })
+        .pipe(mocha());
 });
 
 gulp.task('build-es5', function () {
